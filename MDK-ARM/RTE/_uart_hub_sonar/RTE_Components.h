58a818b1ec83449e86a5a76d3f621eb039bb7938
jinmenglei <229143434@qq.com>
2020-03-17 16:04:02 +0800

    file

---
+++ MDK-ARM/RTE/_uart_hub_sonar/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'uart_hub_sonar' 
+ * Target:  'uart_hub_sonar' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
